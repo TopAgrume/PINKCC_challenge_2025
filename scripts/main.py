308eab7fdea31e5c7e43cc7eae4565bb88583d76
mael <mael.reynaud.pro@gmail.com>
2025-04-05 18:10:07 +0200

    [main] feat: added package building

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,13 @@
-from ocd.dataset.dataset import Dataset
 from ocd.dataset.data_augmentation import DataAugmentation
+from ocd.dataset.dataset import Dataset
 from ocd.dataset.segmentation_dataset_2D import CancerSegmentationDataset
 
+if __name__ == "__main__":
+  datasets = Dataset("DatasetChallenge")
+  train_pairs, val_pairs, test_pairs = datasets.get_dataset_splits()
+  print("Lengths:", len(train_pairs), len(val_pairs), len(test_pairs))
 
-datasets = Dataset('DatasetChallenge')
-train_pairs, val_pairs, test_pairs = datasets.get_dataset_splits()
-print("Lengths:", len(train_pairs), len(val_pairs), len(test_pairs))
-
-transforms = DataAugmentation.get_augmentation_pipeline(
+  transforms = DataAugmentation.get_augmentation_pipeline(
     p=0.5,
     random_crop_scale=(0.85, 1.0),
     h_flip_prob=0.5,
@@ -18,19 +18,20 @@ transforms = DataAugmentation.get_augmentation_pipeline(
     elastic_alpha_range=(1, 2),
     brightness_range=(-0.1, 0.1),
     contrast_range=(0.9, 1.1),
-    gamma_range=(0.9, 1.1)
-)
+    gamma_range=(0.9, 1.1),
+  )
 
-train_dataset = CancerSegmentationDataset(train_pairs)#, transforms=transforms)
-val_dataset = CancerSegmentationDataset(val_pairs)
+  train_dataset = CancerSegmentationDataset(train_pairs)  # , transforms=transforms)
+  val_dataset = CancerSegmentationDataset(val_pairs)
 
-train_loader = CancerSegmentationDataset.get_dataloader(train_dataset, batch_size=16)
-val_loader = CancerSegmentationDataset.get_dataloader(val_dataset, batch_size=16, shuffle=False)
+  train_loader = CancerSegmentationDataset.get_dataloader(train_dataset, batch_size=16)
+  val_loader = CancerSegmentationDataset.get_dataloader(
+    val_dataset, batch_size=16, shuffle=False
+  )
 
+  print("Nb train slices:", len(train_dataset))
+  print("Nb val slices:", len(val_dataset))
 
-print("Nb train slices:", len(train_dataset))
-print("Nb val slices:", len(val_dataset))
-
-data_iter = iter(train_loader)
-batch = next(data_iter)
-print("Batch shapes:", batch[0].shape, batch[1].shape)
+  data_iter = iter(train_loader)
+  batch = next(data_iter)
+  print("Batch shapes:", batch[0].shape, batch[1].shape)
