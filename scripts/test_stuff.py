a739a0cabcc33e28bc1598733cdf4ffb18194211
mael <mael.reynaud.pro@gmail.com>
2025-04-12 21:17:04 +0200

    [main] feat: note data

--- scripts/test_stuff.py
+++ scripts/test_stuff.py
@@ -1,4 +1,6 @@
 # %%
+import numpy as np
+
 from ocd.dataset.dataset import SampleUtils
 
 # %%
@@ -9,3 +11,11 @@ SampleUtils.display_slice(nifti[1], figsize=(10, 6))
 
 # %%
 SampleUtils.display_slice(SampleUtils.normalize_ct(nifti[1]), figsize=(10, 6))
+
+# %%
+nifti = SampleUtils.load_from_path(
+  "../DatasetChallenge/Segmentation/MSKCC/347580.nii.gz"
+)
+# %%
+type(nifti)
+np.unique(nifti[1])
