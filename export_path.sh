3cfd92b2476ada98fd339539f01d9c9690cc26f3
Alex DR <alexandre.devaux-riviere@epita.fr>
2025-04-20 20:07:46 +0200

    add missing export_path bash script

---
+++ export_path.sh
@@ -0,0 +1,8 @@
+
+export nnUNet_data_base="/home/alex/PINKCC_challenge_2025"
+export nnUNet_raw="${nnUNet_data_base}/nnUNet_raw_data"
+export nnUNet_preprocessed="${nnUNet_data_base}/nnUNet_preprocessed"
+export nnUNet_results="${nnUNet_data_base}/nnUNet_results"
+
+#python ocd/dataset/nnUNet/convert_dataset.py
+#nnUNetv2_plan_and_preprocess -d 1 --verify_dataset_integrity
